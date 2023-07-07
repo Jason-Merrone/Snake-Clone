a40c8bac1661f3a841363f8024ca0efcf0680e39
Jason Merrone <jasonmerrone@gmail.com>
2023-07-07 05:57:34 -0600

    Added basic UI, score system and mouse controls

--- script.js
+++ script.js
@@ -15,17 +15,18 @@ function queueSize(){
 }
 
 // Various global variables declared
-let gridSize = 30;
-let gridCoordinates = makeGrid(gridSize);
+let gridSize = 20;
+let gridCoordinates = makeGrid();
 enqueue(Math.floor(Math.random() * (gridSize*gridSize)));
 let apple = Math.floor(Math.random() * (gridSize*gridSize));
 while (apple === queue[0]){
     apple = Math.floor(Math.random() * (gridSize*gridSize));
 }
 let ateApple = false;
+let score = 0;
 let currentDirection = null;
 let alive = true;
-let renderGrid = false;
+let renderGrid = true;
 
 // Game loop
 function gameLoop() {
@@ -42,6 +43,7 @@ function update() {
         handleAppleCollision(false);
     }
     handleSelfCollision();
+    document.getElementById("score").textContent="Score: "+score;
 }
 
 // Render function
@@ -49,7 +51,7 @@ function render() {
     // Displays background
     c.clearRect(0, 0, canvas.width, canvas.height);
 
-    displayGrid(gridSize);
+    displayGrid();
     drawSnake();
     drawApple();
 }
@@ -108,6 +110,7 @@ function handleAppleCollision(headTest){ // Head test is only done during first 
         gridCoordinates[apple][3] = false;
         apple = Math.floor(Math.random() * (gridSize*gridSize));
         ateApple = true;
+        score++;
     }
 
     // This tests if any body segment (Including the head) collided with the apple
@@ -133,15 +136,16 @@ function handleSelfCollision(){
     for(let i = 0; i < queue.length; i++){
         if(queue[0] === queue[i] && i !== 0){
             alive = false;
+            break;
         }
     }
 }
 
 // Code for making grid
-function makeGrid(size){
+function makeGrid(){
     coordinates = [];
-    for(let y = 0; y < canvas.height; y+=(canvas.height/size)){
-        for(let x = 0; x < canvas.width; x+=(canvas.width/size)+0.000000000001){
+    for(let y = 0; y < canvas.height; y+=(canvas.height/gridSize)){
+        for(let x = 0; x < canvas.width; x+=(canvas.width/gridSize)+0.000000000001){
             coordinates.push([x,y,false,false]);
         }
     }
@@ -151,23 +155,67 @@ function makeGrid(size){
 }
 
 // Displays the grid during gameplay
-function displayGrid(size){
+function displayGrid(){
     for(let i = 0; i < gridCoordinates.length; i++){
         if (renderGrid === true){
             c.strokeStyle = "black";
-            c.strokeRect(gridCoordinates[i][0], gridCoordinates[i][1], canvas.width/size, canvas.height/size);
+            c.strokeRect(gridCoordinates[i][0], gridCoordinates[i][1], canvas.width/gridSize, canvas.height/gridSize);
         }
         //Grid spaces containing snake are green
         if(gridCoordinates[i][2] === true){
             c.fillStyle = "green";
-            c.fillRect(gridCoordinates[i][0], gridCoordinates[i][1], canvas.width/size, canvas.height/size);
+            c.fillRect(gridCoordinates[i][0], gridCoordinates[i][1], canvas.width/gridSize, canvas.height/gridSize);
         }
         //Grid spaces containing apple are red
         else if(gridCoordinates[i][3] === true){
             c.fillStyle = "red";
-            c.fillRect(gridCoordinates[i][0], gridCoordinates[i][1], canvas.width/size, canvas.height/size);
+            c.fillRect(gridCoordinates[i][0], gridCoordinates[i][1], canvas.width/gridSize, canvas.height/gridSize);
         }
     }
+}
+
+// Returns the current location of the snake's head (relative to the canvas)
+function findHead(){
+    return [gridCoordinates[queue[0]][0],gridCoordinates[queue[0]][1]]
+}
+
+function reset(){
+    queue = [];
+    gridCoordinates = makeGrid();
+    enqueue(Math.floor(Math.random() * (gridSize*gridSize)));
+    apple = Math.floor(Math.random() * (gridSize*gridSize));
+    while (apple === queue[0]){
+        apple = Math.floor(Math.random() * (gridSize*gridSize));
+    }
+    ateApple = false;
+    score = 0;
+    currentDirection = null;
+    alive = true;
+}
+
+function removeGrid(){
+    if(renderGrid === true){
+        renderGrid = false;
+        document.getElementById("show-grid").textContent="Show Grid";
+    }
+    else{
+        renderGrid = true;
+        document.getElementById("show-grid").textContent="Remove Grid";
+    }
+}
+
+function changeGridSize(direction){
+    if(direction === "up" && gridSize < 99){
+        gridSize++;
+    }
+    else if(direction != "up" && gridSize > 2){
+        gridSize--;
+    }
+    else{
+        return;
+    }
+    reset();
+    document.getElementById("grid-size-text").value="Grid Size: "+gridSize;
 }
 
 // Calls main game loop
@@ -175,6 +223,12 @@ gameLoop();
 
 // Sets an nterval between logic calculations
 let timer = setInterval(update,100);
+
+
+document.getElementById("reset").addEventListener("click", reset);
+document.getElementById("show-grid").addEventListener("click", removeGrid);
+document.getElementById("grid-size-left").addEventListener("click", changeGridSize.bind(null, "down"));
+document.getElementById("grid-size-right").addEventListener("click", changeGridSize.bind(null, "up"));
 
 // Adds an event listener for the keydown event
 document.addEventListener('keydown', function(event) {
@@ -191,4 +245,46 @@ document.addEventListener('keydown', function(event) {
     else if ((key === 'ArrowLeft' || key === 'a' || key === 'A') && !(queue[0] === queue[1]+1)) {
         currentDirection = "left";
     } 
+  })
+
+  // Adds an event mousedown for the keydown event
+  document.getElementById("gameCanvas").addEventListener('mousedown', function(event) {
+    // Calculates the center of the snake's head
+    const headCenterX = findHead()[0] + 0.5 * (canvas.width / gridSize);
+    const headCenterY = findHead()[1] + 0.5 * (canvas.height / gridSize);
+
+    // Finds the relative distance of the latest click from the center of the snake's head
+    const x = event.clientX - document.getElementById("gameCanvas").getBoundingClientRect().left - headCenterX;
+    const y = -(event.clientY - document.getElementById("gameCanvas").getBoundingClientRect().top - headCenterY); // y is negative to make calculations more intuitive to my mathematical nature
+
+    // Calculates where the snake should move when there is only one body segment
+    if(queue.length <= 1){
+        if (Math.abs(x) <= Math.abs(y) && y > 0) {
+            currentDirection = "up";
+        } 
+        else if (Math.abs(x) <= Math.abs(y) && y < 0) {
+            currentDirection = "down";
+        } 
+        else if (Math.abs(x) >= Math.abs(y) && x > 0) {
+            currentDirection = "right";
+        } 
+        else if (Math.abs(x) >= Math.abs(y) && x < 0) {
+            currentDirection = "left";
+        }
+    }
+    // Calculates where the snake should move when there is more than one body segment
+    else {
+        if(y > 0 && (currentDirection === "left" || currentDirection === "right")){
+            currentDirection = "up";
+        }
+        else if(y < 0 && (currentDirection === "left" || currentDirection === "right")){
+            currentDirection = "down";
+        }
+        else if(x > 0 && (currentDirection === "up" || currentDirection === "down")){
+            currentDirection = "right";
+        }
+        else if(x < 0 && (currentDirection === "up" || currentDirection === "down")){
+            currentDirection = "left";
+        }
+    }
   })
